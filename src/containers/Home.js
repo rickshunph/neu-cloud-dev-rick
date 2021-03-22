186cc034df4b44f92825573384ef51a168a35e1e
rickshunph <huangsp97@gmail.com>
2021-03-21 23:26:04 -0700

    add to display a note

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,15 +3,63 @@ import ListGroup from "react-bootstrap/ListGroup";
 import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import "./Home.css";
+import { API } from "aws-amplify";
+import { BsPencilSquare } from "react-icons/bs";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function Home() {
   const [notes, setNotes] = useState([]);
   const { isAuthenticated } = useAppContext();
   const [isLoading, setIsLoading] = useState(true);
 
-  function renderNotesList(notes) {
-    return null;
+useEffect(() => {
+  async function onLoad() {
+    if (!isAuthenticated) {
+      return;
+    }
+
+    try {
+      const notes = await loadNotes();
+      setNotes(notes);
+    } catch (e) {
+      onError(e);
+    }
+
+    setIsLoading(false);
   }
+
+  onLoad();
+}, [isAuthenticated]);
+
+function loadNotes() {
+  return API.get("notes", "/notes");
+}
+
+function renderNotesList(notes) {
+  return (
+    <>
+      <LinkContainer to="/notes/new">
+        <ListGroup.Item action className="py-3 text-nowrap text-truncate">
+          <BsPencilSquare size={17} />
+          <span className="ml-2 font-weight-bold">Create a new note</span>
+        </ListGroup.Item>
+      </LinkContainer>
+      {notes.map(({ noteId, content, createdAt }) => (
+        <LinkContainer key={noteId} to={`/notes/${noteId}`}>
+          <ListGroup.Item action>
+            <span className="font-weight-bold">
+              {content.trim().split("\n")[0]}
+            </span>
+            <br />
+            <span className="text-muted">
+              Created: {new Date(createdAt).toLocaleString()}
+            </span>
+          </ListGroup.Item>
+        </LinkContainer>
+      ))}
+    </>
+  );
+}
 
   function renderLander() {
     return (
